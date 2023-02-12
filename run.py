cfa4ccf5ef755844390d7eae900b657cef812684
rasmus1111 <rasmusareaxelsson@gmail.com>
2023-02-12 05:19:02 +0000

    game

--- run.py
+++ run.py
@@ -1,3 +1,112 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import sys
+
+
+board = [' ' for x in range(9)]
+player_name = input("Enter your name: ").strip()
+
+player_wins = 0
+computer_wins = 0
+ties = 0
+
+def print_board():
+    print("\n")
+    for i in range(3):
+        print(" " + board[i*3] + " | " + board[i*3 + 1] + " | " + board[i*3 + 2] + " ")
+        if i != 2:
+            print("---+---+---")
+    print("\n")
+
+def print_scoreboard():
+    print("\nScoreboard: ")
+    print(player_name + ": " + str(player_wins) + " wins")
+    print("Computer: " + str(computer_wins) + " wins")
+    print("Ties: " + str(ties) + "\n")
+
+def player_move(icon):
+    while True:
+        try:
+            choice = input("Enter your move (1-9) or 'exit' to quit: ").strip()
+            if choice == 'exit':
+                print("Thanks for playing Tic-Tac-Toe! Have a great day.")
+                sys.exit()
+            else:
+                choice = int(choice) - 1
+                if choice >= 0 and choice <= 8:
+                    if board[choice] == ' ':
+                        board[choice] = icon
+                        break
+                    else:
+                        print("\nSpace already taken, try again.")
+                else:
+                    print("\nInvalid choice, try again.")
+        except ValueError:
+            print("\nInvalid input, try again.")
+
+def is_victory(icon):
+    for i in range(3):
+        if board[i*3] == icon and board[i*3 + 1] == icon and board[i*3 + 2] == icon:
+            return True
+        if board[i] == icon and board[i + 3] == icon and board[i + 6] == icon:
+            return True
+    if board[0] == icon and board[4] == icon and board[8] == icon:
+        return True
+    if board[2] == icon and board[4] == icon and board[6] == icon:
+        return True
+    return False
+
+def is_draw():
+    for i in range(9):
+        if board[i] == ' ':
+            return False
+    return True
+
+def computer_move(icon):
+    while True:
+        choice = random.randint(0, 8)
+        if board[choice] == ' ':
+            board[choice] = icon
+            break
+
+print("Welcome to Tic-Tac-Toe, " + player_name + "!\n")
+print("The game is played on a 3x3 grid.")
+print("You will be 'X' and the computer will be 'O'.")
+print("To make a move, enter a number from 1 to 9.")
+print("The numbers correspond to the positions on the grid as follows:\n")
+print(" 1 | 2 | 3 ")
+print("---+---+---")
+print(" 4 | 5 | 6 ")
+print("---+---+---")
+print(" 7 | 8 | 9 ")
+input("Press Enter to start playing: ")
+
+while True:
+    print_board()
+    print_scoreboard()
+    player_move('X')
+    if is_victory('X'):
+        print_board()
+        print("X wins! Congratulations " + player_name + "!")
+        player_wins += 1
+        if player_wins == 5:
+            print("\n" + player_name + " has won the game by 5 points! Congratulations!")
+            break
+        board = [' ' for x in range(9)]
+    elif is_draw():
+        print_board()
+        print("It's a draw")
+        ties += 1
+        if ties == 5:
+            print("\nThe game has ended in a draw by 5 ties.")
+            break
+        board = [' ' for x in range(9)]
+    else:
+        computer_move('O')
+        if is_victory('O'):
+            print_board()
+            print("O wins! Better luck next time " + player_name + ".")
+            computer_wins += 1
+            if computer_wins == 5:
+                print("\nThe computer has won the game by 5 points.")
+                break
+            board = [' ' for x in range(9)]
